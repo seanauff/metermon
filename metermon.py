e07bbbd28d02666d0883730a3939a7fd0a5711b8
Sean Auffinger <sean.auffinger@gmail.com>
2024-05-04 14:06:51 -0600

    update code for paho-mqtt v2

--- metermon.py
+++ metermon.py
@@ -46,16 +46,16 @@ R900_ATTRIBS = {
 }
 
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
+def on_connect(client, userdata, flags, reason_code, properties):
     # print connection statement
-    print(f"Connected to broker at {MQTT_BROKER_HOST}:{MQTT_BROKER_PORT} with result code {rc}: "+mqtt.connack_string(rc))
+    print(f"Connected to broker at {MQTT_BROKER_HOST}:{MQTT_BROKER_PORT} with reason code {reason_code}: "+mqtt.connack_string(reason_code))
 
     # set mqtt status message
     client.publish(MQTT_TOPIC_PREFIX+"/status",payload="Online",qos=1,retain=True)
 
-def on_disconnect(client, userdata, rc):
-    if rc != 0:
-        print(f"Unexpected disconnection from broker (RC={rc}). Attempting to reconnect...")
+def on_disconnect(client, userdata, flags, reason_code, properties):
+    if reason_code != 0:
+        print(f"Unexpected disconnection from broker (RC={reason_code}). Attempting to reconnect...")
 
 # set up mqtt client
 client = mqtt.Client(client_id=MQTT_CLIENT_ID)
