48c3d2c0ee1c0c7357f3bfe24a1136eab728c112
Sean Auffinger <sean.auffinger@gmail.com>
2025-03-19 20:08:18 -0600

    change to ghcr.io

--- multiarch-build.sh
+++ multiarch-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script based on this guide: https://medium.com/@life-is-short-so-enjoy-it/docker-how-to-build-and-push-multi-arch-docker-images-to-docker-hub-64dea4931df9
-docker buildx build --tag seanauff/metermon:latest --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --builder container --push .
+docker buildx build --tag ghcr.io/seanauff/metermon:latest --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --builder container --push .
